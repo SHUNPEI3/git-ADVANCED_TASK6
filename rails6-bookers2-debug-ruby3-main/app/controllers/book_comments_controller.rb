750ae078ba209e6dd910352e74033a483c16274a
EC2 Default User <ec2-user@ip-172-31-33-11.ap-northeast-1.compute.internal>
2022-12-16 09:05:38 +0000

    [Finish]Advanced_Task6

--- rails6-bookers2-debug-ruby3-main/app/controllers/book_comments_controller.rb
+++ rails6-bookers2-debug-ruby3-main/app/controllers/book_comments_controller.rb
@@ -1,22 +1,19 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = BookComment.new(comment_params)
-    comment.user_id = current_user.id
-    comment.book_id = book.id
-    if comment.save
-      redirect_to book_path(book.id)
-    else
-      redirect_to book_path(book.id)
-    end
+    @book = Book.find(params[:book_id])
+    @comment = BookComment.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.book_id = @book.id
+    @comment.save
+    render 'book_comment'
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    comment = BookComment.find_by(id: params[:id], book_id: book.id)
-    comment.destroy
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:book_id])
+    @comment = BookComment.find_by(id: params[:id], book_id: @book.id)
+    @comment.destroy
+    render 'book_comment'
   end
 
   private
--- rails6-bookers2-debug-ruby3-main/app/controllers/favorites_controller.rb
+++ rails6-bookers2-debug-ruby3-main/app/controllers/favorites_controller.rb
@@ -6,12 +6,14 @@ class FavoritesController < ApplicationController
     favorite.user_id = current_user.id
     favorite.book_id = @book.id
     favorite.save
+    render 'favorite'
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     favorite = Favorite.find_by(user_id: current_user.id, book_id: @book.id)
     favorite.destroy
+    render 'favorite'
   end
 
 end
